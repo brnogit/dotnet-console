c6504becf16875a4bca3ee13104dad33a1e2aedd
bmotadev <bmotadev@gmail.com>
2022-08-06 14:43:25 -0300

    DOTNET direto ao ponto - console

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Digite seu nome:");
+
+var nome = Console.ReadLine();
+Console.WriteLine("Hello, " + nome);
+Console.WriteLine($"Hello, {nome}");
+
+string segundoNome = "Mota"; // string usa aspas duplas e o char usa aspas simples
+int numeroInt = 5;
+float numeroFloat = 5.4f;
+double numeroDouble = 5.4;
+decimal numeroDecimal = 5.3m;
+int[] matrizArray = new int[3] { 1, 2, 3};
+char caractere = 'a';
+DateTime hoje = DateTime.Now;
+DateTime aniversario = new DateTime(1991, 1, 1);
+
+// if - else
+
+Console.WriteLine("Digite uma opção entre 1 e 3");
+var opcao = Console.ReadLine();
+
+if (opcao == "1") {
+    Console.WriteLine("if-else: Opção 1 selecionada.");
+} else if (opcao == "2") {
+    Console.WriteLine("if-else: Opção 2 selecionada.");
+} else if (opcao == "3") {
+    Console.WriteLine("if-else: Opção 3 selecionada.");
+} else {
+    Console.WriteLine("if-else: Opção inválida.");
+}
+
+switch (opcao) {
+    case "1":
+        Console.WriteLine("switch-case: Opção 1 selecionada.");
+        break;
+    case "2":
+        Console.WriteLine("switch-case: Opção 2 selecionada.");
+        break;
+    case "3":
+        Console.WriteLine("switch-case: Opção 3 selecionada.");
+        break;
+    default:
+        Console.WriteLine("switch-case: Opção inválida");
+        break;
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+.NET direto ao ponto
